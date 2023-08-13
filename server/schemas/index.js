48bd7549d19910ef9c7fd2f3160eeabc0eff22ac
FRVR-JAE <tjoloyede9@gmail.com>
2023-08-13 02:25:24 -0700

    rn+sanity

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,6 +1,6 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
 
-export const schemaTypes = [post, author, category, blockContent]
+import category from './category'
+import post from './products'
+
+
+export const schemaTypes = [post, category]
